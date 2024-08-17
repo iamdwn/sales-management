ba023d753a3b442d3178cac7a010ca6d450dc2a9
iamdwn <truongdinhdongduong@gmail.com>
2024-08-18 03:41:08 +0700

    feat: add login and register UI

---
+++ PRN221.SalesManagement.Web/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221.SalesManagement.Web.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
