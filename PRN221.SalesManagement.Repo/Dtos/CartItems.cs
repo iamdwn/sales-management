09be54734ba9e93937aa1b582a02ca2cfa05f48f
iamdwn <truongdinhdongduong@gmail.com>
2024-08-19 06:07:51 +0700

    feat: add cart UI

---
+++ PRN221.SalesManagement.Repo/Dtos/CartItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN221.SalesManagement.Repo.Dtos
+{
+    public class CartItems
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+    }
+}
