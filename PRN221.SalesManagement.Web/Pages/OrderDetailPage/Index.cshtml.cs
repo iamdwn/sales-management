d8c03499933ae4db4ee8794337ac4905013f2f27
iamdwn <truongdinhdongduong@gmail.com>
2024-08-20 01:46:27 +0700

    fix: adjust UI of Order Detail

--- PRN221.SalesManagement.Web/Pages/OrderDetailPage/Index.cshtml.cs
+++ PRN221.SalesManagement.Web/Pages/OrderDetailPage/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using PRN221.SalesManagement.Repo.Interfaces;
 using PRN221.SalesManagement.Repo.Models;
 
@@ -8,10 +9,12 @@ namespace PRN221.SalesManagement.Web.Pages.OrderDetailPage
     public class IndexModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly PRN221.SalesManagement.Repo.Persistences.SalesManagementContext _context;
 
-        public IndexModel(IUnitOfWork unitOfWork)
+        public IndexModel(IUnitOfWork unitOfWork, Repo.Persistences.SalesManagementContext context)
         {
             _unitOfWork = unitOfWork;
+            _context = context;
         }
 
         public IList<OrderDetail> OrderDetail { get; set; } = new List<OrderDetail>();
@@ -19,6 +22,7 @@ namespace PRN221.SalesManagement.Web.Pages.OrderDetailPage
         // Pagination properties
         public int PageIndex { get; set; } = 1;
         public int TotalPages { get; set; }
+        public string includeProperties = "SaleOrder,Product";
 
         [BindProperty(SupportsGet = true)]
         public int PageSize { get; set; } = 10; // Default page size
@@ -35,11 +39,24 @@ namespace PRN221.SalesManagement.Web.Pages.OrderDetailPage
                 PageIndex = TotalPages;
             }
 
-            OrderDetail = _unitOfWork.OrderDetailRepository.Get(
-                includeProperties: "Order,Product",
-                pageIndex: PageIndex,
-                pageSize: PageSize
-            ).ToList();
+            //OrderDetail = _unitOfWork.OrderDetailRepository.Get(
+            //    includeProperties: "SaleOrder, Product",
+            //    pageIndex: PageIndex,
+            //    pageSize: PageSize
+            //).ToList();
+            IQueryable<OrderDetail> query = _context.OrderDetails;
+
+            // Include related entities
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            // Apply pagination
+            query = query.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            OrderDetail = query.ToList();
         }
 
         public bool HasPreviousPage => PageIndex > 1;
