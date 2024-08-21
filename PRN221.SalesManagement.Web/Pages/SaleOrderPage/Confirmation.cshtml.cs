9b1e2c6e7e27f5ce520a5fb5d9e28445d3670774
iamdwn <truongdinhdongduong@gmail.com>
2024-08-21 14:07:35 +0700

    feat: add confirmation page

--- PRN221.SalesManagement.Web/Pages/SaleOrderPage/Confirmation.cshtml.cs
+++ PRN221.SalesManagement.Web/Pages/SaleOrderPage/Confirmation.cshtml.cs
@@ -1,35 +1,16 @@
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-//using PRN221.SalesManagement.Repo.Interfaces;
-//using PRN221.SalesManagement.Repo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-//namespace PRN221.SalesManagement.Web.Pages.SaleOrderPage
-//{
-//    public class ConfirmationModel : PageModel
-//    {
-//        private readonly IUnitOfWork _unitOfWork;
+namespace PRN221.SalesManagement.Web.Pages.SaleOrderPage
+{
+    public class ConfirmationModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public int SaleOrderId { get; set; }
 
-//        public ConfirmationModel(IUnitOfWork unitOfWork)
-//        {
-//            _unitOfWork = unitOfWork;
-//        }
-
-//        public SaleOrder SaleOrder { get; set; } = default!;
-//        public List<OrderDetail> OrderDetails { get; set; } = default!;
-
-//        public IActionResult OnGet(int id)
-//        {
-//            SaleOrder = _unitOfWork.SaleOrderRepository.GetByID(id);
-//            if (SaleOrder == null)
-//            {
-//                return NotFound();
-//            }
-
-//            OrderDetails = _unitOfWork.OrderDetailRepository.Get()
-//                .Where(od => od.OrderId == id)
-//                .ToList();
-
-//            return Page();
-//        }
-//    }
-//}
+        public void OnGet(int id)
+        {
+            SaleOrderId = id;
+        }
+    }
+}
